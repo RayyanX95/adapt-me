92f4743b5a901da9f323b7df48a4c4bdfa63a013
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2022-12-03 17:29:42 +0200

    https instead of http for security issue

--- src/useBreed.js
+++ src/useBreed.js
@@ -20,7 +20,7 @@ const useBreedList = (animal) => {
       setStatus("Loading...");
 
       const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+        `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
       );
       const json = await res.json();
       localCache[animal] = json.breeds || [];
